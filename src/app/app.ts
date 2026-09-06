efc852b3b8a72a036d002e8a0b8ef97474c39353
VirendraRaut <virendraraut1306@gmail.com>
2026-09-06 18:39:35 +0530

    angular tutorials

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Rooms } from './rooms/rooms';
-import { NgSwitch, NgSwitchDefault } from "@angular/common";
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Rooms, NgSwitch, NgSwitchDefault],
+  imports: [Rooms, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
