ca590d65182f3e492abf4eed34980eff9fe483b5
VirendraRaut <virendraraut1306@gmail.com>
2026-09-06 19:26:15 +0530

    pipes in the angular

--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NgForOf, NgIf, NgClass } from "@angular/common";
+import { NgForOf, NgIf, NgClass, DatePipe, UpperCasePipe } from "@angular/common";
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.html',
   styleUrl: './rooms.scss',
-  imports: [NgForOf, NgIf, NgClass],
+  imports: [NgForOf, NgIf, NgClass, DatePipe, UpperCasePipe],
 })
 export class Rooms {
   hotelName = 'Marvels';
